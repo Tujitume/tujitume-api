c6159d866605b1672a89ddc767c2ccd74ded217d
Md Nurul Kabir <tronrambo166@gmail.com>
2026-08-22 23:22:52 +0600

    settings

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -31,6 +31,8 @@ class UserResource extends JsonResource
             $this->loadMissing('capital_profile');
         }
 
+        $settings = $this->loadMissing('settings');
+
         $organization = $this->relationLoaded('organization') ? $this->organization : null;
         $organizationWorkspaces = [];
 
@@ -94,6 +96,7 @@ class UserResource extends JsonResource
             'lipr_wallet_account' => $this->lipr_wallet_account,
             'organization' => $organizationPayload,
             'workspaces' => $organizationWorkspaces,
+            'settings' => $this->settings
         ];
 
         if (!$isOrganization) {
