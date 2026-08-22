44833c5260af01ac6484100e32f5fb7fd531fcfc
Md Nurul Kabir <tronrambo166@gmail.com>
2026-08-23 00:21:40 +0600

    fix

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -31,7 +31,7 @@ class UserResource extends JsonResource
             $this->loadMissing('capital_profile');
         }
 
-        $this->load('getSettings');
+        $this->load('settings');
 
         $organization = $this->relationLoaded('organization') ? $this->organization : null;
         $organizationWorkspaces = [];
@@ -96,7 +96,7 @@ class UserResource extends JsonResource
             'lipr_wallet_account' => $this->lipr_wallet_account,
             'organization' => $organizationPayload,
             'workspaces' => $organizationWorkspaces,
-            'settings' => $this->getSettings ?? null,
+            'settings' => $this->getSettings() ?? null,
         ];
 
         if (!$isOrganization) {
